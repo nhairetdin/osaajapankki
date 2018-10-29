501852725f083e86ff511dae167307efc9f891cc
Nico Hairetdin <22236980+nhairetdin@users.noreply.github.com>
2018-10-29 12:24:50 +0200

    Add config file

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBwWHwl06yULGjsIJCEnyMSBLJv8U5X4uA",
+    authDomain: "osaajapankki2.firebaseapp.com",
+    databaseURL: "https://osaajapankki2.firebaseio.com",
+    projectId: "osaajapankki2",
+    storageBucket: "osaajapankki2.appspot.com",
+    messagingSenderId: "284481593658"
+  }
+  
+export default config
