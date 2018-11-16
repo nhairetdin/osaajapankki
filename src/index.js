6dc3d7e5837d31834a74b96ee25e5cd31eca6356
Nico Hairetdin <hairetdin.nico@gmail.com>
2018-11-16 12:19:03 +0200

    Add and use Gotham-font at inputs and table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import './fonts/gotham/style.css'
 
 ReactDOM.render(
   <Provider store={store}>
